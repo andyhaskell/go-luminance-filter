e7501a4d67da5d7e7345ce9f68b3c4a21e540bbe
&y Haskell <andy.with.ampersand@gmail.com>
2020-02-02 17:02:46 -0500

    Add the main loop for recoloring an image
    This function will loop over each pixel of the image similarly to how
    we loop over 2D arrays, and in upcoming commits, for each pixel we will
    check how bright that pixel is, and then recolor it accordingly.

--- main.go
+++ main.go
@@ -9,6 +9,21 @@ import (
 	"os"
 )
 
+func recolor(img image.Image) {
+	b := img.Bounds()
+
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			color := img.At(x, y)
+
+			// we have the color of the pixel at (x, y). Now we
+			// just need to figure out how bright that pixel is,
+			// and recolor it accordingly!
+			_ = color
+		}
+	}
+}
+
 func main() {
 	f, err := os.Open("raw.jpg")
 	if err != nil {
@@ -20,6 +35,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("error decoding file to an Image: %v", err)
 	}
-	_ = img // TODO: Edit the image
-	log.Println("We got an image!")
+
+	// we need this to know the coordinates of the top-left pixel, which isn't
+	// necessarily (0, 0)
+	b := img.Bounds()
+
+	color := img.At(b.Min.X, b.Min.Y)
+	log.Printf("%#v\n", color)
 }
