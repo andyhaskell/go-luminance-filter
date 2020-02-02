6d2d60b209764a7a26862f3b911d1331b725a65b
&y Haskell <andy.with.ampersand@gmail.com>
2020-02-02 18:50:02 -0500

    Add function for getting the luminance of a pixel
    We pass in a color.Color from each pixel we're reading, then we use
    the luminance formula to determine if it is lighter or darker than a
    50% threshold. In the next commit, we'll use that information to
    recolor the image we're running this code on.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"image"
+	"image/color"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
@@ -9,19 +10,38 @@ import (
 	"os"
 )
 
+func luminancePercent(c color.Color) float64 {
+	r, g, b, _ := c.RGBA()
+
+	// We're dividing our pixel's red, green, and blue values by 2^16 because
+	// in colors returned from Color.RGBA(), the maximum value for a color
+	// is 2^16-1, or 65,535.
+	redPercent := float64(r) / 65535 * 100
+	greenPercent := float64(g) / 65535 * 100
+	bluePercent := float64(b) / 65535 * 100
+
+	return redPercent*0.2126 + greenPercent*0.7152 + bluePercent*0.0722
+}
+
 func recolor(img image.Image) {
 	b := img.Bounds()
 
+	var lightPixels, darkPixels int
 	for y := b.Min.Y; y < b.Max.Y; y++ {
 		for x := b.Min.X; x < b.Max.X; x++ {
 			color := img.At(x, y)
 
-			// we have the color of the pixel at (x, y). Now we
-			// just need to figure out how bright that pixel is,
-			// and recolor it accordingly!
-			_ = color
+			// [TODO] Replace this with actually setting the color
+			// of the current pixel.
+			if luminancePercent(color) > 50 {
+				lightPixels++
+			} else {
+				darkPixels++
+			}
 		}
 	}
+	log.Printf("This image has %d pixels above 50%% luminance and %d below 50%%\n",
+		lightPixels, darkPixels)
 }
 
 func main() {
@@ -36,10 +56,5 @@ func main() {
 		log.Fatalf("error decoding file to an Image: %v", err)
 	}
 
-	// we need this to know the coordinates of the top-left pixel, which isn't
-	// necessarily (0, 0)
-	b := img.Bounds()
-
-	color := img.At(b.Min.X, b.Min.Y)
-	log.Printf("%#v\n", color)
+	recolor(img)
 }
