829ea31ada7ba9e140e050cffe7c396af7d2faf4
&y Haskell <andy.with.ampersand@gmail.com>
2020-02-08 17:58:43 -0500

    Use image.Set to recolor the image, output the result with jpeg.Encode
    This commit replaces the code for counting light and dark pixels with
    code that recolors pixels, applying them to an image.RGBA that is then
    returned from the recolor function.
    
    Additionally, before outputting the result of the recolor function to
    the recolored.jpg file, we have a dialogue asking whether the user
    wants to replace the recolored.jpg file if it already exists.

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"image"
 	"image/color"
 	_ "image/gif"
-	_ "image/jpeg"
+	"image/jpeg"
 	_ "image/png"
 	"log"
 	"os"
+	"strings"
 )
 
 func luminancePercent(c color.Color) float64 {
@@ -23,25 +26,26 @@ func luminancePercent(c color.Color) float64 {
 	return redPercent*0.2126 + greenPercent*0.7152 + bluePercent*0.0722
 }
 
-func recolor(img image.Image) {
+func recolor(img image.Image) image.Image {
 	b := img.Bounds()
+	recolored := image.NewRGBA(b)
 
-	var lightPixels, darkPixels int
+	// Add color.Color variables for the colors we're recoloring to
+	charcoal := color.RGBA{R: 34, G: 31, B: 32, A: 255}
+	blueGreen := color.RGBA{R: 128, G: 201, B: 172, A: 255}
+
 	for y := b.Min.Y; y < b.Max.Y; y++ {
 		for x := b.Min.X; x < b.Max.X; x++ {
 			color := img.At(x, y)
 
-			// [TODO] Replace this with actually setting the color
-			// of the current pixel.
 			if luminancePercent(color) > 50 {
-				lightPixels++
+				recolored.Set(x, y, charcoal)
 			} else {
-				darkPixels++
+				recolored.Set(x, y, blueGreen)
 			}
 		}
 	}
-	log.Printf("This image has %d pixels above 50%% luminance and %d below 50%%\n",
-		lightPixels, darkPixels)
+	return recolored
 }
 
 func main() {
@@ -56,5 +60,30 @@ func main() {
 		log.Fatalf("error decoding file to an Image: %v", err)
 	}
 
-	recolor(img)
+	if _, err := os.Stat("recolored.jpg"); err != nil && !os.IsNotExist(err) {
+		log.Fatalf("unexpected error checking if recolored.jpg already exists")
+	} else if err == nil {
+		fmt.Println("recolored.jpg already exists. Replace it? (y/n)")
+
+		replace, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Fatalf(
+				"unexpected error reading whether to replace the file: %v",
+				err,
+			)
+		} else if strings.TrimRight(strings.ToLower(replace), "\n") != "y" {
+			return
+		}
+	}
+
+	out, err := os.Create("recolored.jpg")
+	if err != nil {
+		log.Fatalf("error creating output file recolored.jpg: %v", err)
+	}
+	defer out.Close()
+
+	recolored := recolor(img)
+	if err := jpeg.Encode(out, recolored, nil); err != nil {
+		log.Fatalf("error outputting recolored image: %v", err)
+	}
 }
