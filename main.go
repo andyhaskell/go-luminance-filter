994b6a9ce099519c8b860aaede58a5572fcfa3ee
&y Haskell <andy.with.ampersand@gmail.com>
2020-02-08 19:12:49 -0500

    Move code for recoloring image to own file

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"image"
-	"image/color"
 	_ "image/gif"
 	"image/jpeg"
 	_ "image/png"
@@ -14,42 +13,6 @@ import (
 
 	"github.com/spf13/cobra"
 )
-
-// luminancePercent calculates the relative luminance of a pixel,
-func luminancePercent(c color.Color) float64 {
-	r, g, b, _ := c.RGBA()
-
-	// We're dividing our pixel's red, green, and blue values by 2^16 because
-	// in colors returned from Color.RGBA(), the maximum value for a color
-	// is 2^16-1, or 65,535.
-	redPercent := float64(r) / 65535 * 100
-	greenPercent := float64(g) / 65535 * 100
-	bluePercent := float64(b) / 65535 * 100
-
-	return redPercent*0.2126 + greenPercent*0.7152 + bluePercent*0.0722
-}
-
-func recolor(img image.Image) image.Image {
-	b := img.Bounds()
-	recolored := image.NewRGBA(b)
-
-	// Add color.Color variables for the colors we're recoloring to
-	charcoal := color.RGBA{R: 34, G: 31, B: 32, A: 255}
-	blueGreen := color.RGBA{R: 128, G: 201, B: 172, A: 255}
-
-	for y := b.Min.Y; y < b.Max.Y; y++ {
-		for x := b.Min.X; x < b.Max.X; x++ {
-			color := img.At(x, y)
-
-			if luminancePercent(color) > 50 {
-				recolored.Set(x, y, charcoal)
-			} else {
-				recolored.Set(x, y, blueGreen)
-			}
-		}
-	}
-	return recolored
-}
 
 var cmd = &cobra.Command{
 	Use:   "go-luminance-filter raw.jpg -o recolored.jpg -t 0,50 -c 80C9AC,221F20",
